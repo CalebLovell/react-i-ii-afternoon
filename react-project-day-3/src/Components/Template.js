d90bce6c0d38c9b551046171ec47ac7fa31cf270
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-14 09:09:31 -0600

    make work !

--- react-project-day-3/src/Components/Template.js
+++ react-project-day-3/src/Components/Template.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import Infobox from './Infobox';
 import Navigator from './Navigator';
+import Data from '../Data';
 
 class Template extends Component {
     constructor() {
         super();
 
         this.state = {
-            index: 1,
+            Data,
+            index: 1
         }
     }
 
     handleClickNext = () => {
         if (this.state.index < 25) {
-            this.setState({index: this.state.index+1})
+            this.setState({ index: this.state.index + 1 })
             console.log(`Click increased Counter`)
-        } else if (this.state.index = 25) {
-            this.setState({index: 1})
-            console.log(`Reset Counter`)
+        } else if (this.state.index === 25) {
+            this.setState({ index: 1 })
+            console.log(`Reset Counter!`)
         } else {
             console.log(`Click could not increase Counter`)
         }
     }
-
+    
     handleClickPrev = () => {
         if (this.state.index > 1) {
-            this.setState({index: this.state.index-1})
+            this.setState({ index: this.state.index - 1 })
             console.log(`Click decreased Counter`)
+        } else if (this.state.index === 1) {
+            this.setState({ index: 25 })
+            console.log(`Go to end!`)
         } else {
             console.log(`Click could not decrease Counter`)
         }
@@ -39,8 +43,23 @@ class Template extends Component {
                     <h1 className="Home">Home</h1>
                 </header>
                 <body className="Body">
-                    <Infobox />
-                    <Navigator number={this.state.index} handleClickPrev={this.handleClickPrev} handleClickNext={this.handleClickNext}/>
+                    <div className="Infobox">
+                        <div className="info">
+                            <h2>{Data[this.state.index-1].name.first} {Data[this.state.index-1].name.last}</h2>
+                            <div className="info-specs-1">
+                                <h3>From: {Data[this.state.index-1].country}</h3>
+                                <h3>Job Title: {Data[this.state.index-1].title} </h3>
+                                <h3>Employer: {Data[this.state.index-1].employer}</h3>
+                            </div>
+                            <div className="info-specs-2">
+                                <h3>Favorite Movies: </h3>
+                                <li>{Data[this.state.index-1].favoriteMovies[0]}</li>
+                                <li>{Data[this.state.index-1].favoriteMovies[1]}</li>
+                                <li>{Data[this.state.index-1].favoriteMovies[2]}</li>
+                            </div>
+                        </div>
+                    </div>
+                    <Navigator number={this.state.index} handleClickPrev={this.handleClickPrev} handleClickNext={this.handleClickNext} />
                 </body>
             </div>
         );
